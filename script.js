05d6c3a9086452ef5e21adf8f3a476ba05cd3801
RobFenger <robfenger@gmail.com>
2021-09-22 14:11:34 +0200

    almost final

--- script.js
+++ script.js
@@ -1,45 +1,104 @@
+let iconContainer = document.getElementById("icon-container");
+let dropdownMenu = document.getElementById("dropdown-menu");
+
+iconContainer.onclick = () => {
+  if (dropdownMenu.style.visibility === 'hidden') {
+    dropdownMenu.style.visibility = 'visible';
+  } else {
+    dropdownMenu.style.visibility = 'hidden';
+  }
+}
+
+document.onmouseup = (event) => {
+  if (event.target !== dropdownMenu) {
+    dropdownMenu.style.visibility = 'hidden';
+  }
+}  
+
+
+
+
 let down = document.getElementById("down");
-let main = document.getElementById("main");
+let page = document.getElementById("page");
+let up = document.getElementById("up");
+let front = document.getElementById("front");
+
+let a = window.matchMedia("(max-height: 700px)");
+let b = window.matchMedia("(max-height: 600px)");
+let c = window.matchMedia("(max-height: 500px)");
+let d = window.matchMedia("(max-height: 400px)");
+let e = window.matchMedia("(max-height: 300px)");
+let f = window.matchMedia("(max-height: 200px)");
+
+
 
 down.onclick = () => {
-  main.style.top = '55px';
+  page.style.top = '55px';
 }
-
-let up = document.getElementById("up");
 
 up.onclick = () => {
-  main.style.top = '650px';
+  if (f.matches) {
+    page.style.top = '200px';
+  } else if (e.matches) {
+    page.style.top = '300px';
+  } else if (d.matches) {
+    page.style.top = '400px';
+  } else if (c.matches) {
+    page.style.top = '500px';
+  } else if (b.matches) {
+    page.style.top = '600px';
+  } else if (a.matches) {
+    page.style.top = '700px';
+  } 
 }
+
 
 
 let darkButton = document.getElementById("dark");
 let lightButton = document.getElementById("light");
 let toggle = document.getElementById("toggle");
-/*let header = document.getElementById("header");
-let front = document.getElementById("front");
-let home = document.getElementById("menu-item");
-let menu = document.getElementsByClassName("menu-item");*/
+let header = document.getElementById("header");
+let menuItems = document.getElementsByClassName("menu-item");
+let codecademy = document.getElementById("codecademy");
+
 
 darkButton.onclick = () => {
-  main.style.color = 'white';
-  main.style.backgroundColor = 'black';
+  page.style.color = 'snow';
+  page.style.backgroundColor = 'black';
   toggle.style.border = 'solid white 1px';
-  up.style.color = 'lightblue';
-  /*header.style.backgroundColor = 'black';
-  menu.style.color = "white";
-  front.style.backgroundColor = "black";*/
+  up.style.color = 'snow';
+  header.style.backgroundColor = 'black';
+  codecademy.style.color = 'snow';
+  let i;
+  for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].style.color = 'snow';
+  };
 }
 
 lightButton.onclick = () => {  
-  main.style.color = 'black';
-  main.style.backgroundColor = 'snow';
+  page.style.color = 'black';
+  page.style.backgroundColor = 'snow';
   toggle.style.border = 'solid black 1px';
-  up.style.color = "indigo";
-  /*header.style.backgroundColor = 'snow';
-  menu.style.color = "black";
-  front.style.backgroundColor = "snow";*/
+  up.style.color = "black";
+  header.style.backgroundColor = 'snow';
+  codecademy.style.color = 'black';
+  let i;
+  for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].style.color = 'black';
+  }
 }
 
+up.onmouseenter = () => {
+  up.style.color = 'lightseagreen';
+}
+
+up.onmouseleave = () => {
+  if (page.style.backgroundColor === 'snow') {
+    up.style.color = 'black';
+  } else {
+    up.style.color = 'snow';
+  }
+}
 
 
 
@@ -59,95 +118,96 @@ let quote = document.getElementById('winningquote');
 let currentlyPlaying = true;
 
 function isJoker (card) {
-    if (card.src === joker) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  if (card.src === joker) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-  function isClicked (card) {
-    if (card.src === closedCard) {
-      return false;
-    } else {
-      return true;
-    }
-  };
+function isClicked (card) {
+  if (card.src === closedCard) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-  function playCard (card) {
-    numClosedCards--;
-    if (numClosedCards === 0) {
-      gameOver('win');
-    } else if (isJoker(card)) {
-      gameOver();
-    }
-  };
+function playCard (card) {
+  numClosedCards--;
+  if (numClosedCards === 0) {
+    gameOver('win');
+  } else if (isJoker(card)) {
+    gameOver();
+  }
+};
 
-  let randomCardGenerator = () => {
-    let card = Math.floor(Math.random() * numClosedCards);
-    if (card === 0) {
-      openCard1 = joker;
-      openCard2 = clubs;
-      openCard3 = hearts;
-    } else if (card === 1) {
-      openCard2 = joker;
-      openCard1 = clubs;
-      openCard3 = hearts;
-    } else if (card === 2) {
-      openCard3 = joker;
-      openCard1 = hearts;
-      openCard2 = clubs;
-    }
-  };
+let randomCardGenerator = () => {
+  let card = Math.floor(Math.random() * numClosedCards);
+  if (card === 0) {
+    openCard1 = joker;
+    openCard2 = clubs;
+    openCard3 = hearts;
+  } else if (card === 1) {
+    openCard2 = joker;
+    openCard1 = clubs;
+    openCard3 = hearts;
+  } else if (card === 2) {
+    openCard3 = joker;
+    openCard1 = hearts;
+    openCard2 = clubs;
+  }
+};
 
-  cardImage1.onclick = () => {
-    if (currentlyPlaying === true && !isClicked(cardImage1)) {
-      cardImage1.src = openCard1;
-    playCard(cardImage1);
-    }
-  };
+cardImage1.onclick = () => {
+  if (currentlyPlaying === true && !isClicked(cardImage1)) {
+    cardImage1.src = openCard1;
+  playCard(cardImage1);
+  }
+};
 
-  cardImage2.onclick = () => {
-    if (currentlyPlaying === true && !isClicked(cardImage2)) {
-    cardImage2.src = openCard2;
-    playCard(cardImage2);
-    }
-  };
+cardImage2.onclick = () => {
+  if (currentlyPlaying === true && !isClicked(cardImage2)) {
+  cardImage2.src = openCard2;
+  playCard(cardImage2);
+  }
+};
   
-  cardImage3.onclick = () => {
-    if (currentlyPlaying === true && !isClicked(cardImage3)) {
-    cardImage3.src = openCard3;
-    playCard(cardImage3);
-    }
-  };
+cardImage3.onclick = () => {
+  if (currentlyPlaying === true && !isClicked(cardImage3)) {
+  cardImage3.src = openCard3;
+  playCard(cardImage3);
+  }
+};
 
-  startButton.onclick = () => {
-    if (!currentlyPlaying) {
-  startRound();
-  }};
+startButton.onclick = () => {
+  if (!currentlyPlaying) {
+startRound();
+  }
+};
 
-  function startRound () {
-    cardImage1.src = closedCard;
-    cardImage2.src = closedCard;
-    cardImage3.src = closedCard;
-    numClosedCards = 3;
-    startButton.innerHTML = 'Good Luck!';
-    quote.innerHTML = '~';
-    currentlyPlaying = true;
-    randomCardGenerator();
-  };
+function startRound () {
+  cardImage1.src = closedCard;
+  cardImage2.src = closedCard;
+  cardImage3.src = closedCard;
+  numClosedCards = 3;
+  startButton.innerHTML = 'Good Luck!';
+  quote.innerHTML = '~';
+  currentlyPlaying = true;
+  randomCardGenerator();
+};
 
-  function gameOver (status) {
-    if (status === 'win') {
-      startButton.innerHTML = 'You win! Play again?';
-      quote.innerHTML = '~Why so serious?~'
-    } else {
-      startButton.innerHTML = 'Game over! Play again?';
-      quote.innerHTML = "~Life's a gamble, better take your chance~";
-    }
-    currentlyPlaying = false;
-  };
+function gameOver (status) {
+  if (status === 'win') {
+    startButton.innerHTML = 'You win! Play again?';
+    quote.innerHTML = '~Why so serious?~'
+  } else {
+  startButton.innerHTML = 'Game over! Play again?';
+  quote.innerHTML = "~Life's a gamble, better take your chance~";
+  }
+  currentlyPlaying = false;
+};
 
-  startRound();
+startRound();
 
   
